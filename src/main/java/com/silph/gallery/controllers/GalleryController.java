ce6707049964ae031d6bab922e6b97db08d5a91d
quellovecchio <l.delvecchio@live.it>
2019-06-09 16:41:22 +0200

    Created login page

--- src/main/java/com/silph/gallery/controllers/GalleryController.java
+++ src/main/java/com/silph/gallery/controllers/GalleryController.java
@@ -1,11 +1,18 @@
 package com.silph.gallery.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.silph.gallery.model.Employee;
 import com.silph.gallery.services.AlbumService;
 import com.silph.gallery.services.PhotoService;
 import com.silph.gallery.services.PhotographerService;
@@ -28,6 +35,24 @@ public class GalleryController {
     
     @GetMapping("/login")
     public String login (Model model) {
-    	return "login.html";
+    	return "pages/login.html";
     }
+    
+    @RequestMapping(value = "/loginEmployee", method = RequestMethod.POST)
+    public String getStudente(@Valid @ModelAttribute("employee") Employee employee, Model model, BindingResult br) {
+    	this.employeeValidator.validate(employee, br);
+    	if (!br.hasErrors()) {
+    		model.addAttribute("currentEmployee", employee);
+    		return "pages/employeeDashboard.html";
+    	}
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- src/main/java/com/silph/gallery/model/Photographer.java
+++ src/main/java/com/silph/gallery/model/Photographer.java
@@ -17,6 +17,19 @@ public class Photographer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	@OneToMany(mappedBy = "photographer", fetch = FetchType.LAZY)
+	private List<Album> albums;
+	@Column
+	private String name;
+	@Column
+	private String surname;
+	
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 	public long getId() {
 		return id;
 	}
@@ -35,9 +48,4 @@ public class Photographer {
 	public void setName(String name) {
 		this.name = name;
 	}
-	@OneToMany(mappedBy = "photographer", fetch = FetchType.LAZY)
-	private List<Album> albums;
-	@Column
-	private String name;
-	
 }
